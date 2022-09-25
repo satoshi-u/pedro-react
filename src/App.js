3f515bc6809433a43655c93e5cffec98d273e00e
Sarthak Joshi <sarthak@settlemint.com>
2022-09-25 20:26:51 +0530

    pedro#2 : intro to JSX, react-components, component-props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  // const userJSX = (
+  //   <div className="App-header">
+  //     <h1>Sarthak</h1>
+  //     <h2>sarthak@settlemint.com</h2>
+  //     <h3>Belgium</h3>
+  //   </div>
+  // );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {userJSX} */}
+      <User
+        name="Sarthak"
+        email="sarthak@settlemint.com"
+        location="Belgium"
+        age={25}
+      />
+      <User
+        name="Pedro"
+        email="pedro@settlemint.com"
+        location="Belgium"
+        age={21}
+      />
     </div>
   );
 }
+
+const User = (props) => {
+  return (
+    <div className="App-header">
+      <h1>{props.name}</h1>
+      <h2>{props.email}</h2>
+      <h3>{props.location}</h3>
+      <h4>{props.age}</h4>
+    </div>
+  );
+};
 
 export default App;
