0bae746e35d7dbda037cf50671cd956e0b0069fc
satoshi-u <sarthakjoshi.in@gmail.com>
2022-09-26 10:54:37 +0530

    pedro#3 : css modules, conditional rendering, working with lists

--- src/App.js
+++ src/App.js
@@ -1,43 +1,64 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.css';
+import { User } from './User';
 
+// npx create-react-app .
 function App() {
-  // const userJSX = (
-  //   <div className="App-header">
-  //     <h1>Sarthak</h1>
-  //     <h2>sarthak@settlemint.com</h2>
-  //     <h3>Belgium</h3>
-  //   </div>
-  // );
+  const isCompanyOpen = true;
+  const locations = ['Belgium', 'Dubai', 'Singapore', 'Delhi'];
+  const users = [
+    {
+      name: 'Sarthak',
+      email: 'sarthak@settlemint.com',
+      location: 'Delhi',
+      age: 25,
+    },
+    {
+      name: 'Nandan',
+      email: 'nandan@settlemint.com',
+      location: 'Belgium',
+      age: 35,
+    },
+  ];
 
   return (
-    <div className="App">
-      {/* {userJSX} */}
-      <User
-        name="Sarthak"
-        email="sarthak@settlemint.com"
-        location="Belgium"
-        age={25}
-      />
-      <User
-        name="Pedro"
-        email="pedro@settlemint.com"
-        location="Belgium"
-        age={21}
-      />
+    <div className={styles.App}>
+      {users.map((user, key) => {
+        return (
+          <User
+            name={user.name}
+            email={user.email}
+            location={user.location}
+            age={user.age}
+          />
+        );
+      })}
+      <div className={styles.Footer}>
+        {isCompanyOpen ? (
+          <h1 style={{ color: 'green' }}>open for business</h1>
+        ) : (
+          <h2 style={{ color: 'red' }}>check back later</h2>
+        )}
+        {isCompanyOpen && <button>LogIn</button>}
+        <h2 style={{ color: isCompanyOpen ? 'blue' : 'yellow' }}>
+          Settlemint Pvt. Ltd.
+        </h2>
+        <div>
+          <h1>Contact Us</h1>
+          <div className={styles.ContactUs}>
+            {locations &&
+              locations.map((location, key) => {
+                return (
+                  <h2 key={key} style={{ margin: '0px 50px' }}>
+                    {location}
+                  </h2>
+                );
+              })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-const User = (props) => {
-  return (
-    <div className="App-header">
-      <h1>{props.name}</h1>
-      <h2>{props.email}</h2>
-      <h3>{props.location}</h3>
-      <h4>{props.age}</h4>
-    </div>
-  );
-};
 
 export default App;
