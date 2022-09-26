591424da4e4e2de50162d3d020f95b07ae61f6aa
satoshi-u <sarthakjoshi.in@gmail.com>
2022-09-26 14:30:54 +0530

    pedro#4 : working with react states & useState hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import styles from './App.module.css';
 import { User } from './User';
+import { Footer } from './Footer';
 
 // npx create-react-app .
 function App() {
-  const isCompanyOpen = true;
-  const locations = ['Belgium', 'Dubai', 'Singapore', 'Delhi'];
   const users = [
     {
       name: 'Sarthak',
@@ -33,30 +32,7 @@ function App() {
           />
         );
       })}
-      <div className={styles.Footer}>
-        {isCompanyOpen ? (
-          <h1 style={{ color: 'green' }}>open for business</h1>
-        ) : (
-          <h2 style={{ color: 'red' }}>check back later</h2>
-        )}
-        {isCompanyOpen && <button>LogIn</button>}
-        <h2 style={{ color: isCompanyOpen ? 'blue' : 'yellow' }}>
-          Settlemint Pvt. Ltd.
-        </h2>
-        <div>
-          <h1>Contact Us</h1>
-          <div className={styles.ContactUs}>
-            {locations &&
-              locations.map((location, key) => {
-                return (
-                  <h2 key={key} style={{ margin: '0px 50px' }}>
-                    {location}
-                  </h2>
-                );
-              })}
-          </div>
-        </div>
-      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import styles from './App.module.css';
+
+export const Footer = () => {
+  // DATA
+  const locations = ['Belgium', 'Dubai', 'Singapore', 'Delhi'];
+
+  // STATE
+  const [isCompanyOpen, setIsCompanyOpen] = useState(true);
+  const [countProjects, setCountProjects] = useState(0);
+  const [email, setEmail] = useState('');
+  const [showEamilInput, setShowEamilInput] = useState(true);
+
+  // ACTION
+  const handleEmailChange = (event) => {
+    console.log('event.target.value: ', event.target.value);
+    setEmail(event.target.value);
+  };
+
+  // RENDER
+  return (
+    <div className={styles.Footer}>
+      <div className={styles.Company}>
+        <h1 style={{ color: isCompanyOpen ? '#61dafb' : '#F7E8A0' }}>
+          Settlemint - Blockchain Technology made Simple
+        </h1>
+        <div className={styles.UnionRow}>
+          {isCompanyOpen ? (
+            <h2 style={{ color: '#A5E0AB' }}>open for business :)</h2>
+          ) : (
+            <h2 style={{ color: '#EC635A' }}>check back later :(</h2>
+          )}
+          <button
+            onClick={() => {
+              setIsCompanyOpen(!isCompanyOpen);
+            }}
+          >
+            {isCompanyOpen ? 'CLOSE' : 'OPEN'}{' '}
+          </button>
+        </div>
+        <div className={styles.UnionRow}>
+          <h3>Projects Delivered : {countProjects}</h3>
+          {
+            <button
+              onClick={() => {
+                setCountProjects(countProjects + 1);
+              }}
+            >
+              ++
+            </button>
+          }
+        </div>
+      </div>
+      <div className={styles.ContactUs}>
+        <p>Contact Us (email)</p>
+        <input type="text" onChange={handleEmailChange}></input>
+        <button onClick={() => setShowEamilInput(!showEamilInput)}>
+          show
+        </button>
+        {showEamilInput && <p>{email}</p>}
+      </div>
+      <div className={styles.Locations}>
+        {locations &&
+          locations.map((location, key) => {
+            return (
+              <h3 key={key} style={{ margin: '0px 50px' }}>
+                {location}
+              </h3>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
